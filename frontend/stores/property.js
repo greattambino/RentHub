83b373e822428a649d3d39a4c8eee82cf072fecd
greattambino <marctambara@gmail.com>
2016-03-21 01:16:05 -0700

    parse properties by location

--- frontend/stores/property.js
+++ frontend/stores/property.js
@@ -3,19 +3,40 @@ var AppDispatcher = require('../dispatcher/dispatcher');
 var PropertyConstants = require('../constants/property_constants');
 var PropertyStore = new Store(AppDispatcher);
 var _properties = [];
+var _CAProperties = [];
+var _WAProperties = [];
 
 var _resetProperties = function (properties) {
   _properties = properties.slice(0);
+};
+var _updateLocations = function () {
+  var californiaProperties = [];
+  var washingtonProperties = [];
+  _properties.map(function (property) {
+    if (property.state === "CA") {
+      californiaProperties.push(property);
+    } else {
+      washingtonProperties.push(property);
+    }
+  });
+  _CAProperties = californiaProperties;
+  _WAProperties = washingtonProperties;
 };
 
 PropertyStore.all = function () {
   return _properties.slice(0);
 };
-
+PropertyStore.washington = function () {
+  return _WAProperties.slice(0);
+};
+PropertyStore.california = function () {
+  return _CAProperties.slice(0);
+};
 PropertyStore.__onDispatch = function (payload) {
   switch (payload.actionType) {
     case PropertyConstants.PROPERTIES_RECEIVED:
       _resetProperties(payload.properties);
+      _updateLocations();
       PropertyStore.__emitChange();
       break;
   }
