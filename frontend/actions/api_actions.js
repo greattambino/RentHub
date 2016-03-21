a9fe660b268e4f1d516a55cbf19ee31dfc12d480
greattambino <marctambara@gmail.com>
2016-03-21 00:56:23 -0700

    add action for active property

--- frontend/actions/api_actions.js
+++ frontend/actions/api_actions.js
@@ -1,11 +1,18 @@
 var PropertyConstants = require('../constants/property_constants');
 var AppDispatcher     = require('../dispatcher/dispatcher');
+var ActiveConstants   = require('../constants/active_constants');
 
 ApiActions = {
   receiveProperties: function (properties) {
     AppDispatcher.dispatch({
       actionType: PropertyConstants.PROPERTIES_RECEIVED,
       properties: properties
+    });
+  },
+  updateActiveProperty: function (property) {
+    AppDispatcher.dispatch({
+      actionType: ActiveConstants.PROPERTY_RECEIVED,
+      property: property
     });
   }
 };
