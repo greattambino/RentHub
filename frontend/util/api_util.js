c56b9728efa5af16ef107769ff59dffe7a30aa82
greattambino <marctambara@gmail.com>
2016-03-21 01:24:24 -0700

    add fetchProperties function for filtering by keyword

--- frontend/util/api_util.js
+++ frontend/util/api_util.js
@@ -13,6 +13,30 @@ ApiUtil = {
         console.log(error);
       }
     });
+  },
+  fetchProperties: function (keyword) {
+    $.ajax({
+      url: "http://www.onerent.co/api/Property/availableProperties",
+      type: "POST",
+      dataType: 'json',
+      success: function (response) {
+        var results = [];
+        outer:
+        for (var i in response) {
+          inner:
+          for (var j in response[i].images){
+            if (response[i].images[j].labels[keyword]) {
+              results.push(response[i]);
+              break inner;
+            }
+          }
+        }
+        ApiActions.receiveProperties(results);
+      },
+      error: function (error) {
+        console.log(error);
+      }
+    });
   }
 };
 
