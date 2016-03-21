fd12b759c197cc0f8360db5343fdd0b24b09175e
greattambino <marctambara@gmail.com>
2016-03-21 01:15:22 -0700

    update for active location state

--- frontend/stores/active.js
+++ frontend/stores/active.js
@@ -3,10 +3,15 @@ var AppDispatcher   = require('../dispatcher/dispatcher');
 var ActiveConstants = require('../constants/active_constants');
 var ActiveStore     = new Store(AppDispatcher);
 var _activeProperty = -1;
+var _activeLocation = "All";
 
 var _resetActiveProperty  = function () { _activeProperty = -1; };
+var _updateActiveLocation = function (location) { _activeLocation = location; };
 var _updateActiveProperty = function (property) { _activeProperty = property; };
 
+ActiveStore.activeLocation = function () {
+  return _activeLocation;
+};
 ActiveStore.activeProperty = function () {
   return _activeProperty;
 };
@@ -17,7 +22,12 @@ ActiveStore.__onDispatch = function (payload) {
       ActiveStore.__emitChange();
       break;
     case ActiveConstants.PROPERTY_RECEIVED:
-      _updateActiveProperty(payload.property);
+      _updateActiveProperty(payload.property, payload.location);
+      ActiveStore.__emitChange();
+      break;
+    case ActiveConstants.LOCATION_RECEIVED:
+      _updateActiveLocation(payload.location);
+      _activeProperty = -1;
       ActiveStore.__emitChange();
       break;
   }
